42f8818d2c1e8c4da978c95f4974c7684777f2a2
Satria Yudha A. H <73085599+Haru3119@users.noreply.github.com>
2023-10-01 22:27:20 +0700

    Update tgs2c.go

--- tgs2c.go
+++ tgs2c.go
@@ -3,9 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var x float64
-	fmt.Scan(&x)
+	var x int
+	fmt.Print("Enter a positive integer (less than or equal to 999): ")
+	_, err := fmt.Scan(&x)
 
-	var fx = ( ( x * x * x + 3 * x ) / ( x * x * x *x - 3 * x * x + 4) )
-	fmt.Print(fx)
+	if err != nil || x < 1 || x > 999 {
+		fmt.Println("Input is not within the valid range (1 to 999).")
+		return
+	}
+
+	d1 := x / 100       
+	d2 := (x / 10) % 10 
+	d3 := x % 10        
+
+	fmt.Printf("The first digit is %d\n", d1)
+	fmt.Printf("The second digit is %d\n", d2)
+	fmt.Printf("The third digit is %d\n", d3)
 }
